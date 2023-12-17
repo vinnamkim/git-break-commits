8fa903a83a832b6a3da319b00a6fdf866759eee0
Vinnam Kim <vinnam.kim@gmail.com>
2023-12-17 14:51:10 +0900

    Add ratatui.rs
    Signed-off-by: Vinnam Kim <vinnam.kim@gmail.com>

---
+++ examples/crossterm.rs
@@ -0,0 +1,24 @@
+use std::io::{self, Write};
+use crossterm::{
+    ExecutableCommand, QueueableCommand,
+    terminal, cursor, style::{self, Stylize}
+};
+
+fn main() -> io::Result<()> {
+  let mut stdout = io::stdout();
+
+  stdout.execute(terminal::Clear(terminal::ClearType::All))?;
+
+  for y in 0..40 {
+    for x in 0..150 {
+      if (y == 0 || y == 40 - 1) || (x == 0 || x == 150 - 1) {
+        // in this loop we are more efficient by not flushing the buffer.
+        stdout
+          .queue(cursor::MoveTo(x,y))?
+          .queue(style::PrintStyledContent( "█".magenta()))?;
+      }
+    }
+  }
+  stdout.flush()?;
+  Ok(())
+}
